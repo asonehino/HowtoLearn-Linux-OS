c4b0ba4f9e24c8d785fc81a320c26c80efd8de68
hino <asonehino@naver.com>
2024-10-16 09:18:53 +0900

    h

--- ch02/ch02_01.c
+++ ch02/ch02_01.c
@@ -3,10 +3,10 @@
 #include <sys/stat.h>
 
 int main(void) {
-    if (mkdir("han", 0755) == -1) {
-        perror("han");
-        exit(1);
-    }
-    printf("directory exists\n");
-    return 0;
+  if (mkdir("han", 0755) == -1) {
+    perror("han");
+    exit(1);
+  }
+  printf("directory exists\n");
+  return 0;
 }
